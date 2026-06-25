c0b223374987e4aef3706605253ad1fc7d45589f
kalvohh <kayikalvin@gmail.com>
2026-06-25 14:34:59 +0300

    blurriness

--- src/components/Projectdetail.jsx
+++ src/components/Projectdetail.jsx
@@ -752,7 +752,7 @@ function DetailHero({ project }) {
             backgroundImage: `url(${project.image})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
-            opacity: 0.7,
+            opacity: 0.65,
             filter: "blur(8px)",
             scale: imageScale,
           }}
